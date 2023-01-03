1d5bdbec4a3029ef56c1aede22b638cd2b8ffde6
Austin Miller <austinmillerwebdev@gmail.com>
2023-01-03 13:41:05 -0500

    Tweak profile, logout link styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
-  get "/profile", to: "users#profile", as: :user
+  get "/profile", to: "users#profile"
   devise_for :users
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
